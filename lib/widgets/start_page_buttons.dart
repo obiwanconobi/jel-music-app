4559a4f2f8fab2c5265f01f4871e0b928ac18012
obiwanconobi <connerpanaro@gmail.com>
2024-08-24 12:08:42 +0100

    added buttons back

--- lib/widgets/start_page_buttons.dart
+++ lib/widgets/start_page_buttons.dart
@@ -30,37 +30,225 @@ class _StartPageButtonsState extends State<StartPageButtons> {
   Widget build(BuildContext context) {
     return Visibility(
       visible: visible,
-      child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: 45.w, // Set the desired width here
-              height: 12.w,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LikedSongs()),
-                  );
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
-                      child: Icon(Icons.favorite, size: 24, color:Theme.of(context).textTheme.bodyMedium!.color ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
-                      child: Text(
-                          'Liked Songs',
-                          style: Theme.of(context).textTheme.bodyMedium
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(children:
+          [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 45.w, // Set the desired width here
+                height: 12.w,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ArtistPage()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: SvgPicture.asset(
+                          'assets/svg/artist.svg',
+                          width: 24,
+                          height: 24,
+                          color: Theme.of(context).textTheme.bodyMedium!.color,
+                        ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                            'Artists',
+                            style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 45.w, // Set the desired width here
+                height: 12.w,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AllAlbumsPage(favourite: false,)),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: SvgPicture.asset(
+                          'assets/svg/song.svg',
+                          width: 24,
+                          height: 24,
+                          color: Theme.of(context).textTheme.bodyMedium!.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                            'Albums',
+                            style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 45.w, // Set the desired width here
+                height: 12.w,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DownloadsPage()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      /*Padding(
+                                          padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                          child: Icon(Icons.save, color: Theme.of(context).textTheme.bodyLarge!.color, size:24)
+                                        ),*/
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                            'Downloads',
+                            style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+          ],),
+          Column(children:
+          [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 45.w, // Set the desired width here
+                height: 12.w,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LikedSongs()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: Icon(Icons.favorite, size: 24, color:Theme.of(context).textTheme.bodyMedium!.color ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                            'Liked Songs',
+                            style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 45.w, // Set the desired width here
+                height: 12.w,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AllSongsPage()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: SvgPicture.asset(
+                          'assets/svg/album.svg',
+                          width: 24,
+                          height: 24,
+                          color: Theme.of(context).textTheme.bodyMedium!.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                            'All Songs',
+                            style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 45.w, // Set the desired width here
+                height: 12.w,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const PlaylistsPage()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: SvgPicture.asset(
+                          'assets/svg/album.svg',
+                          width: 24,
+                          height: 24,
+                          color: Theme.of(context).textTheme.bodyMedium!.color,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: Text(
+                            'Playlists',
+                            style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],)
+        ],
+      )
     );
   }
 }
