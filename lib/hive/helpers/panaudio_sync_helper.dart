6b45763fc9a942f2534cea2a7ae6f199d598fa8a
obiwanconobi <connerpanaro@gmail.com>
2024-12-07 16:46:32 +0000

    fix disc number sync issue

--- lib/hive/helpers/panaudio_sync_helper.dart
+++ lib/hive/helpers/panaudio_sync_helper.dart
@@ -31,15 +31,16 @@ class PanaudioSyncHelper implements ISyncHelper {
       await songsHelper.openBox();
       var songs = await panaudioHandler.returnSongs();
       for(var song in songs){
-        var addSong = Songs(id: song["id"],name: song["title"], artist: song["artist"], artistId: song["artistId"], album: song["album"], albumId: song["albumId"], favourite: song["favourite"], index: song["trackNumber"], playCount: song["playCount"], length: song["length"], year: 1900, codec: song["codec"], bitdepth: song["bitDepth"], discIndex: 0, downloaded: false, bitrate: song["bitRate"], samplerate: song["sampleRate"]);
+        var addSong = Songs(id: song["id"],name: song["title"], artist: song["artist"], artistId: song["artistId"], album: song["album"], albumId: song["albumId"], favourite: song["favourite"], index: song["trackNumber"], playCount: song["playCount"], length: song["length"], year: 1900, codec: song["codec"], bitdepth: song["bitDepth"], discIndex: song["discNumber"] ?? 1, downloaded: false, bitrate: song["bitRate"], samplerate: song["sampleRate"]);
         var result = songsHelper.returnSong(addSong.artist, addSong.name);
         if(result == null){
           songsHelper.addSongToBox(addSong);
           count++;
         }else{
-          if((addSong.playCount != result.playCount) || (addSong.favourite != result.favourite)){
+          if((addSong.playCount != result.playCount) || (addSong.favourite != result.favourite) || (addSong.discIndex != result.discIndex)){
             result.playCount = addSong.playCount;
             result.favourite = addSong.favourite;
+            result.discIndex = addSong.discIndex;
            songsHelper.updateSong(result);
            count++;
           }
